51094677d4d620203fed4e6fe3192329593f5b07
kijiwon <kiji1313@gmail.com>
2025-08-21 17:47:33 +0900

    component test

--- App.tsx
+++ App.tsx
@@ -1,45 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Button, Image, Text, TouchableOpacity, View } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <View>
+      <Text>hi 나는 죠니다 이녀석들아</Text>
+      {/* <Image
+        source={require('./image.png')}
+        style={{ width: '50%', height: '50%' }}
+      /> */}
+      <Image
+        source={{ uri: 'https://reactnative.dev/img/homepage/dissection.png' }}
+        style={{ width: '50%', height: '50%' }}
       />
+      <Button title="button" />
+
+      {/* 터치할 수 있는 공간 */}
+      <TouchableOpacity onPress={() => console.log('touch!')}>
+        <Text>touchable</Text>
+      </TouchableOpacity>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+};
 
 export default App;
