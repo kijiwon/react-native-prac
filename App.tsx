3d5e6f5d3f8cc548a2f285ee27b9238959e9a316
kijiwon <kiji1313@gmail.com>
2025-08-22 18:17:14 +0900

    styled-components 적용

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import { Text, View } from 'react-native';
+import { styled } from 'styled-components/native';
+
+const Card = styled.View`
+  background-color: #e2e2e2;
+  padding: 5%;
+  margin: 5%;
+  border-radius: 10px;
+`;
 
 const App = () => {
   return (
     <View>
       <Text>hi 나는 죠니다 이녀석들아</Text>
-      <View
-        style={{
-          backgroundColor: '#e2e2e2',
-          padding: '5%',
-          margin: '5%',
-          borderRadius: 10,
-        }}
-      >
+      <Card>
         <Text style={{ fontSize: 22 }}>Title</Text>
         <Text>This is a description</Text>
-      </View>
+      </Card>
     </View>
   );
 };
