060863c4762c2a6cc9949985d0ff608d0788c2f7
kijiwon <kiji1313@gmail.com>
2025-08-25 23:07:31 +0900

    basic componenet

--- weatherApp/app/(tabs)/index.tsx
+++ weatherApp/app/(tabs)/index.tsx
@@ -1,13 +1,52 @@
-import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    // View는 기본적으로 flex container -> display:flex 할 필요x, 기본 direction은 column
-    <View style={{ flex: 1 }}>
-      {/* 부모 컨테이너를 기준으로 flex 비율을 결정 */}
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 2, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Suwon</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>30</Text>
+          <Text style={styles.description}>Rain</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "black",
+    fontSize: 58,
+    fontWeight: "700",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 20,
+    fontSize: 150,
+    fontWeight: "800",
+  },
+  description: {
+    fontSize: 50,
+    marginTop: -20,
+  },
+});
